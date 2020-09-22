069038c2a009c27b126b9faa1de2dbb956458b60
Emily <linemily97@gmail.com>
2020-09-22 15:39:11 -0700

    updated parameters, send data

--- routes/jobData.js
+++ routes/jobData.js
@@ -9,7 +9,11 @@ router.get("/", (req, res) => {
   res.send("On the Job Data page");
 });
 
-router.get("/:zipCode", async (req, res) => {
+router.get("/:city/:state", async (req, res) => {
+  let city = req.params.city;
+  // city = city.replace(" ", "+");
+
+  let state = req.params.state;
   var skillsArray = [
     // "C",
     "C%23", //C#
@@ -25,13 +29,18 @@ router.get("/:zipCode", async (req, res) => {
   ];
   let resultList = [];
   let item = "";
-  let zipCode = req.params.zipCode;
+  // let zipCode = req.params.zipCode;
   let resultObj = {};
   //   let skill = "python";
-
-  skillsArray
-    .forEach(async skill => {
-      let url = "https://www.indeed.com/jobs?q=" + skill + "&l=" + zipCode;
+  async function fetchSkills() {
+    skillsArray.forEach(async skill => {
+      let url =
+        "https://www.indeed.com/jobs?q=" +
+        skill +
+        "&l=" +
+        city +
+        "%2C+" +
+        state;
 
       async function fetchHTML(fetchUrl) {
         const { data } = await axios.get(fetchUrl);
@@ -44,22 +53,47 @@ router.get("/:zipCode", async (req, res) => {
         // .find("div > div > a")
         .each(function(index, element) {
           item = $(element).text();
-          console.log(item);
+          // console.log(item);
           item = item.match(/of.{1,}job/);
           item = item[0].replace(",", "").match(/\d{1,}/);
         });
-      console.log(item[0]);
+      // console.log(item[0]);
       resultList.push(parseInt(item[0]));
       console.log(resultList);
       resultObj[skill] = parseInt(item[0]);
-
       console.log(resultObj);
-    })
-    .then(result => {
-      console.log(result);
-      res.json(resultObj);
+      // console.log(resultObj);
+      // return resultObj;
+      if (Object.keys(resultObj).length == 10) {
+        res.send(resultObj);
+        return;
+      }
     });
+    // .then(result => {
+    //   console.log(result);
+    //   // res.json(resultObj);
+    // })
+    // .catch(error => {
+    //   console.error(error);
+    // });
 
-  //   res.json(resultObj);
+    // res.send(resultObj);
+    console.log("resultOBj: " + resultObj);
+    return resultObj;
+  }
+
+  const savedObj = await fetchSkills()
+    .then(doc => {
+      console.log("here");
+      console.log(doc);
+      // resc.json(doc);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  // console.log("fetchskills: " + (await fetchSkills()));
+  // // console.log("test");
+  // //   res.json(resultObj);
+  // res.send(resultObj);
 });
 module.exports = router;
