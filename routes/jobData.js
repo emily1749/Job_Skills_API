f6fcdab9316d2bbd786b45da5a28fab72c8ec068
Emily <linemily97@gmail.com>
2020-09-22 23:36:13 -0700

    updated jobData scrape

--- routes/jobData.js
+++ routes/jobData.js
@@ -32,65 +32,62 @@ router.get("/:city/:state", async (req, res) => {
   // let zipCode = req.params.zipCode;
   let resultObj = {};
   //   let skill = "python";
-  async function fetchSkills() {
-    skillsArray.forEach(async skill => {
-      let url =
-        "https://www.indeed.com/jobs?q=" +
-        skill +
-        "&l=" +
-        city +
-        "%2C+" +
-        state;
+  // async function fetchSkills() {
+  skillsArray.forEach(async skill => {
+    let url =
+      "https://www.indeed.com/jobs?q=" + skill + "&l=" + city + "%2C+" + state;
 
-      async function fetchHTML(fetchUrl) {
-        const { data } = await axios.get(fetchUrl);
-        return cheerio.load(data);
-      }
-      const $ = await fetchHTML(url);
-      //   console.log(`Site HTML: ${${"#searchCountPages"}}`);
+    async function fetchHTML(fetchUrl) {
+      const { data } = await axios.get(fetchUrl);
+      return cheerio.load(data);
+    }
+    const $ = await fetchHTML(url);
+    //   console.log(`Site HTML: ${${"#searchCountPages"}}`);
 
-      $('div[id="searchCountPages"]')
-        // .find("div > div > a")
-        .each(function(index, element) {
-          item = $(element).text();
-          // console.log(item);
-          item = item.match(/of.{1,}job/);
-          item = item[0].replace(",", "").match(/\d{1,}/);
-        });
-      // console.log(item[0]);
-      resultList.push(parseInt(item[0]));
-      console.log(resultList);
-      resultObj[skill] = parseInt(item[0]);
-      console.log(resultObj);
-      // console.log(resultObj);
+    $('div[id="searchCountPages"]')
+      // .find("div > div > a")
+      .each(function(index, element) {
+        item = $(element).text();
+        // console.log(item);
+        item = item.match(/of.{1,}job/);
+        item = item[0].replace(",", "").match(/\d{1,}/);
+      });
+    // console.log(item[0]);
+    resultList.push(parseInt(item[0]));
+    console.log(resultList);
+    resultObj[skill] = parseInt(item[0]);
+    console.log(resultObj);
+    // console.log(resultObj);
+    // return resultObj;
+    if (Object.keys(resultObj).length == 10) {
+      res.send(resultObj);
       // return resultObj;
-      if (Object.keys(resultObj).length == 10) {
-        res.send(resultObj);
-        return;
-      }
-    });
-    // .then(result => {
-    //   console.log(result);
-    //   // res.json(resultObj);
-    // })
-    // .catch(error => {
-    //   console.error(error);
-    // });
+    }
+  });
+  // .then(result => {
+  //   console.log(result);
+  //   // res.json(resultObj);
+  // })
+  // .catch(error => {
+  //   console.error(error);
+  // });
 
-    // res.send(resultObj);
-    console.log("resultOBj: " + resultObj);
-    return resultObj;
-  }
+  // res.send(resultObj);
+  // console.log("resultOBj: " + resultObj);
+  // return resultObj;
+  // }
 
-  const savedObj = await fetchSkills()
-    .then(doc => {
-      console.log("here");
-      console.log(doc);
-      // resc.json(doc);
-    })
-    .catch(error => {
-      console.error(error);
-    });
+  // const savedObj = await fetchSkills()
+  //   .then(doc => {
+  //     console.log("here");
+  //     // console.log(doc);
+  //     // resc.json(doc);
+  //   })
+  //   .catch(error => {
+  //     console.error(error);
+  //   });
+
+  // console.log("savedobj:" + savedObj);
   // console.log("fetchskills: " + (await fetchSkills()));
   // // console.log("test");
   // //   res.json(resultObj);
