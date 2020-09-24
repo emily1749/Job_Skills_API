1e7f301a92e368300a8caf9217e67875921cb100
Emily <linemily97@gmail.com>
2020-09-24 00:27:32 -0700

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const jobDataRoute = require("./routes/jobData");
+var cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
